8893844315a48eb7f0fb5eb48e3f02097227e0fe
Daniel Abdelsamed <daniel@abdelsamed.com>
2023-11-19 22:35:25 -0500

    Site name change

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "GQL🅱",
+  tagline: "The Typescript GraphQL query builder of your dreams",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "GQL🅱",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
